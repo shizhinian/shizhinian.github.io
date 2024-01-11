a545d3036fbc7d3fe3f800cfdac155d7f98e07f7
XuRui <378007071@qq.com>
2024-01-11 23:37:43 +0800

    20240111 finish 3-images-x laytout

--- global-top.vue
+++ global-top.vue
@@ -1,19 +1,20 @@
 <template>
     <footer class=" absolute top-0 left-0 right-0 py-0 px-4 flex">
-        <div class=" w-1/3">一级标题内容</div>
-        <div class=" w-1/3">二级标题内容</div>
+        <div class=" w-1/3"></div>
+        <div class=" w-1/3"></div>
         <div class=" w-1/3 flex justify-end">
-            <img src="D:\OneDrive\Obsidian\MyRepository\附件\NNU校徽校名.png" class="my-1 h-12"/>
+            <img src="D:\OneDrive\Obsidian\MyRepository\附件\NNU校徽校名.png" class="my-1 h-12" />
         </div>
     </footer>
-    
-    <!-- <footer class="bg-[#a68f53] absolute bottom-0 left-0 right-0 py-0.5 px-4 flex">
+
+    <footer class="bg-[#00653d] absolute bottom-0 left-0 right-0 py-0.5 px-4 flex">
         <div class="w-1/2">
             NNU
 
         </div>
         <div class="w-1/2 flex justify-end">
-            <SlideCurrentNo/> / <SlidesTotal />
+            <SlideCurrentNo /> /
+            <SlidesTotal />
         </div>
-    </footer> -->
-  </template>
+    </footer>
+</template>
---
+++ styles/index.ts
@@ -0,0 +1,6 @@
+// styles/index.ts
+
+// import './base.css'
+// import './code.css'
+// import './layouts.css'
+import './3-image-x.css'
